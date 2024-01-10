d8e1d83c2172d3db0024950b3f3655a3e8c1ecc6
unknown <its.saran.pv@gmail.com>
2024-01-10 17:44:11 +0530

    Test welcome page navigation

--- src/lib/components/Setup/Setup.svelte
+++ src/lib/components/Setup/Setup.svelte
@@ -1,5 +1,5 @@
-<div class="h-screen flex flex-col items-center justify-center overflow-hidden gap-12">
-    <div class="flex items-center justify-center flex-col gap-4 w-3/4">
+<div class="h-screen flex flex-col items-center">
+    <div class="flex items-center justify-center flex-col gap-4 h-full w-full">
         <div class="avatar">
             <div class="w-52 rounded-full">
                 <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
@@ -7,13 +7,14 @@
         </div>
         <label class="form-control w-full max-w-xs">
             <div class="label">
-              <span class="label-text">What is your name?</span>
+                <span class="label-text">What is your name?</span>
             </div>
             <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
         </label>
     </div>
-    <button class="btn btn-primary w-36">Next</button>
+    <!-- <button class="btn btn-primary w-36">Next</button> -->
+    <div class="join grid grid-cols-2 mt-auto w-full">
+        <button class="join-item btn">Back</button>
+        <button class="join-item btn btn-primary">Next</button>
+    </div>
 </div>
-
-
-
