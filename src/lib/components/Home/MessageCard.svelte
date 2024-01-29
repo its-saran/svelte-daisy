94df345e853ee997a3c9e8eadb8cedb550cbeb25
unknown <its.saran.pv@gmail.com>
2024-01-29 11:07:46 +0530

    Test colors

--- src/lib/components/Home/MessageCard.svelte
+++ src/lib/components/Home/MessageCard.svelte
@@ -26,7 +26,7 @@
     }
 
     .message {
-        @apply bg-neutral border border-neutral font-normal rounded-md py-4 px-4;
+        @apply font-normal rounded-md py-4 px-4;
         /* #F5F5F5 */
     }
 
